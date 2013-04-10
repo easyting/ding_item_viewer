254f4acdb96a2395111e72b2815be87553fe52eb
Alex Semeniuc <as@inlead.dk>
2013-04-10 17:42:52 +0300

    Case 2492 - Made modifications for no-js.

--- templates/ding_item_viewer.content.tpl.php
+++ templates/ding_item_viewer.content.tpl.php
@@ -2,75 +2,37 @@
 /**
  * @file
  * Template file for viewer layout.
+ *
+ * Variables:
+ *   $data - Array containing array of items to show. Has key as tab number.
+ *   $tabs - HTML of tabs. See ding_item_viewer.tabs.tpl.php.
  */
 ?>
 <div class="browsebar-inner">
   <div class="browsebar-items-wrapper">
-    <div class="browsebar-item even first">
-      <div class="image-rating-wrapper">
-        <img src="" class="image"alt="" />
-        <div class="rating rating-0"></div>
-      </div>
-      <span class="title"></span>
-      <span class="author"></span>
-      <span class="more-info"><?php print t('More info'); ?></span>
-    </div>
-    <div class="browsebar-item odd">
-      <div class="image-rating-wrapper">
-        <img src="" class="image" alt="" />
-        <div class="rating rating-0"></div>
-      </div>
-      <span class="title"></span>
-      <span class="author"></span>
-      <span class="more-info"><?php print t('More info'); ?></span>
-    </div>
-    <div class="browsebar-item even">
-      <div class="image-rating-wrapper">
-        <img src="" class="image" alt="" />
-        <div class="rating rating-0"></div>
-      </div>
-      <span class="title"></span>
-      <span class="author"></span>
-      <span class="more-info"><?php print t('More info'); ?></span>
-    </div>
-    <div class="browsebar-item active">
-      <div class="active-item-title"><h2><a href="#" class="active-title active-more-info"></a></h2></div>
-      <div class="cover-wrapper">
-        <div class="cover-wrapper-inner"><img src="" class="image" alt="" /></div>
-        <div class="reserve-container">
-          <div class="item-loan">
-            <?php print l(t('Reserve'), '#'); ?>
-          </div>
-        </div>
-      </div>
-      <div class="properties-wrapper">
-        <span class="active-author"></span>
-        <span class="active-description"></span>
-        <span class="genre"><?php print t('Genre'); ?>: <a href="#"></a></span>
-        <div class="active-rating rating-0"></div><span class="rating-count">(0)</span>
-        <a href="#" class="reviews"><?php print t('Reviews'); ?> <span class="review-count">(0)</span></a>
-        <a href="#" class="active-more-info"><?php print t('More info'); ?></a>
-      </div>
-    </div>
-    <div class="browsebar-item even">
-      <div class="image-rating-wrapper">
-        <img src="" class="image" alt="" />
-        <div class="rating rating-0"></div>
-      </div>
-      <span class="title"></span>
-      <span class="author"></span>
-      <span class="more-info"><?php print t('More info'); ?></span>
-    </div>
-    <div class="browsebar-item odd last">
-
-    <div class="image-rating-wrapper">
-      <img src="" class="image" alt="" />
-      <div class="rating rating-0"></div>
-    </div>
-    <span class="title"></span>
-    <span class="author"></span>
-    <span class="more-info"><?php print t('More info'); ?></span>
-  </div>
+  <?php
+    $i=0;
+    $last = 6; // Fixed number of items to show.
+    foreach($data[0] as $item) {
+      if ($i > $last) {
+        break;
+      }
+      $row_class = ($i % 2 == 0) ? 'even' : 'odd';
+      if ($i == 0) {
+        $row_class .= ' first';
+      }
+      if ($i == $last) {
+        $row_class .= ' last';
+      }
+      if ($i == 3) {
+        echo theme('ding_item_viewer_item_active', array('item' => $item, 'item_class' => $row_class));
+      }
+      else {
+        echo theme('ding_item_viewer_item', array('item' => $item, 'item_class' => $row_class));
+      }
+      $i++;
+    }
+  ?>
   </div>
 </div>
 <?php print $tabs; ?>
